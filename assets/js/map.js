6982881b26ca7bff4988a5734cb84675db0ca0e6
duhaime <douglas.duhaime@gmail.com>
2016-06-03 11:22:08 -0400

    several updates to map to create basemap and center on New Haven green

--- assets/js/map.js
+++ assets/js/map.js
@@ -5,14 +5,27 @@ var globalMap = '';
 var initializeMap = function() {
 
   // specify the coordinates on which to center the map initially
-  centerCoordinates = new L.LatLng(48.85, 7.85);
+  centerCoordinates = new L.LatLng(41.307, -72.928);
 
   // create the map object itself
   var map = new L.Map("map", {
     center: centerCoordinates,
-    zoom: 5,
-    maxZoom: 10,
-    minZoom: 3,
+    zoom: 17,
+    zoomControl: false
   });
+
+  L.control.zoom({
+    position: 'topright',
+    zoom: 17,
+    maxZoom: 20,
+    minZoom: 12,
+  }).addTo(map);
+
+  map.addLayer(new L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+  }));
+
 };
   
