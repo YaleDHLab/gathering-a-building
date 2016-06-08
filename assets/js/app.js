63733a599a03b2f47ead875a886a12d1ce76abce
duhaime <douglas.duhaime@gmail.com>
2016-06-08 17:39:28 -0400

    reformat app and pass page template url dynamically

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,29 @@
-var App = angular.module("App", []);
+var BuildingApp = angular.module("BuildingApp", []);
+
+// filter to escape urls
+BuildingApp.filter('cleanUrl', function() {
+  return window.encodeURIComponent;
+});
+
 
 // Navigation Controller to populate navigation
-App.controller("NavigationController", [
-  "$scope", "$http",
+BuildingApp.controller("NavigationController", [
+      "$scope", "$http",
   function($scope, $http) {
-
 
   }
 ]);
 
 
 // Brand Controller to populate brand
-App.controller("BrandController", [
-  "$scope", "$http",
+BuildingApp.controller("BrandController", [
+      "$scope", "$http",
   function($scope, $http) {
 
     // populate footer fields
     $scope.brand = {
-      "title": "GATHERING A BUILDING"
+      "title": "GATHERING A BUILDING",
+      "logo": "/assets/images/dh-lab-gray.png"
     };
 
   }
@@ -25,8 +31,8 @@ App.controller("BrandController", [
 
 
 // Page Controller to control page content
-App.controller("PageController", [
-  "$scope", "$http",
+BuildingApp.controller("PageController", [
+      "$scope", "$http",
   function($scope, $http) {
 
     // populate footer fields
@@ -34,6 +40,12 @@ App.controller("PageController", [
       "right": "Next &uarr;",
       "left": "Left footer"
     };
+
+    $scope.page = {
+      "url": "/templates/home.html"
+    };
+
+
 
   }
 ]);
