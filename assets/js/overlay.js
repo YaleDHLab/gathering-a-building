41f897d439293100c78b99b508920826e1edfa0d
duhaime <douglas.duhaime@gmail.com>
2016-06-08 09:16:51 -0400

    update overlay js to position multiple elements

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -3,17 +3,38 @@ var resizeOverlay = function() {
 
   // coefficients calculated by taking desired div width 
   // divided by window height, averaging multiple observations
-  var width = windowHeight * .161;
-  var height = windowHeight * .083;
-  var left = windowHeight * .645;
-  var bottom = (windowHeight + 210) * .3035;
 
-  console.log(height, width, left);
+  /***
+  * Overlay 1
+  ***/
+
+  var overlayOneWidth = windowHeight * .161;
+  var overlayOneHeight = windowHeight * .083;
+  var overlayOneLeft = windowHeight * .645;
+  var overlayOneBottom = (windowHeight + 210) * .3035;
+
+  /***
+  * Overlay 2
+  ***/
+
+  var overlayTwoLeft = (windowHeight - 19) * 1.452;
+  var overlayTwoBottom = (windowHeight + 210) * .21;
+
+  /***
+  * Build styles
+  ***/
+
   $(".home-overlay-1").css({
-    "height": height,
-    "width": width,
-    "left": left,
-    "bottom": bottom
+    "height": overlayOneHeight,
+    "width": overlayOneWidth,
+    "left": overlayOneLeft,
+    "bottom": overlayOneBottom
+  });
+
+
+  $(".home-overlay-2").css({
+    "left": overlayTwoLeft,
+    "bottom": overlayTwoBottom
   });
 
   // add listener inside the document load
