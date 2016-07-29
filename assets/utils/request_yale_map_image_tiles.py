ca27d1dc76c34a210d0b0c757187f850c4e1a516
duhaime <douglas.duhaime@gmail.com>
2016-07-29 09:49:13 -0400

    update yale image request script to generalize some features initially assumed to be static

--- assets/utils/request_yale_map_image_tiles.py
+++ assets/utils/request_yale_map_image_tiles.py
@@ -2,33 +2,36 @@ from __future__ import division
 from collections import defaultdict
 from skimage import io
 import numpy as np
-import os, glob
+import os, glob, sys
 
 """This script requests all of the tiles required to build up a 
 given jp2 image, and after fetching each of those tiles, rebuilds
 the retrieved images into one large jp2 file"""
 
-def request_image_tiles(image_id):
+def request_image_tiles(query_root, image_id, offset=0, dry_run=0):
   """Read in an image id (e.g. 15691352) and request each of 
   the image tiles for the current image"""
 
   # the query root contains the base url for queries. One must add
   # x,y to the query for it to resolve, where x = zoom depth {1:5}
   # and y is the tile index
-  query_root = "http://brbl-zoom.library.yale.edu/fcgi-bin/iipsrv.fcgi?FIF=SML_MAPS_TRANSFER/2/"
-  query_root += str(image_id) + "/" + str(image_id) + "jp2&CNT=1&SDS=0,90&JTL="
+  query_root += str(image_id) + "/" + str(image_id) + ".jp2&CNT=1&SDS=0,90&JTL="
+
+  if dry_run:
+    print query_root
+    sys.exit()
 
   if not os.path.exists(out_dir):
     os.mkdir(out_dir)
 
-  for i in xrange(1000):
+  for i in xrange(tiles_to_request):
     try:
-      query = 'wget "' + query_root + str(zoom_depth) + ',' + str(i) 
-      query += '" -O ' + out_dir + "/" + str(i) + ".jp2"
+      query = 'wget "' + query_root + str(zoom_depth) + ',' + str(i+offset)
+      query += '" -O ' + out_dir + "/" + str(i+offset) + ".jp2"
 
       print "request image from:", query 
-
       os.popen(query)
+
     except Exception as exc:
       print exc
 
@@ -46,11 +49,11 @@ def combine_image_tiles():
     # if a request did not fetch an image, it will be < 1000 bytes
     size = os.path.getsize(f)
 
-    if size < 1000:
+    if size < 700:
       continue
 
     else:
-      
+
       # fetch the image index position
       index_position = int( os.path.basename(f).split(".jp2")[0] )
 
@@ -64,7 +67,6 @@ def combine_image_tiles():
       # read the jp2 data into memory as a numpy array
       jp2_array = io.imread(f, plugin='freeimage')
 
-
       d[row_index][column_index] = jp2_array 
 
   # with all the arrays in memory, build the master image
@@ -74,13 +76,15 @@ def combine_image_tiles():
     combined_row = []
 
     for column in d[row].iterkeys():
-      
+
       combined_row.append(d[row][column])
 
     # combine the arrays horizontally
     combined_array = np.hstack(combined_row)
 
     master_image.append(combined_array)
+
+    #io.imsave(str(row) + ".png", combined_array)
 
   # combine all the rows into a single image
   master_array = np.vstack(master_image)
@@ -90,20 +94,27 @@ def combine_image_tiles():
     
  
 if __name__ == "__main__":
+
+  # specify the query root for the url to fetch
+  query_root = "http://brbl-zoom.library.yale.edu/fcgi-bin/iipsrv.fcgi?FIF=SML_MAPS_TRANSFER/7/"
+
   # specify the zoom depth to fetch
-  zoom_depth = 5
+  zoom_depth = 6
+
+  # specify the number of tiles to request
+  tiles_to_request = 2000
 
   # specify the output directory
   out_dir = "yale_map_image_tiles"
 
   # specify the image id to fetch
-  image_id = 15691352
+  image_id = "15691437"
 
-  request_image_tiles(image_id)
+  request_image_tiles(query_root, image_id, offset=0)
 
   # once the individual images are retrieved, manually examine
   # them to see how many images are placed on each row 
   # (i.e. the first image with black border on its left)
-  images_per_row = 23
+  images_per_row = 34
 
-  combine_image_tiles()
+  #combine_image_tiles()
