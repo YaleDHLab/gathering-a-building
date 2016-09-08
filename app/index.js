97346a9d691ce69ebea0f0e08438cb6a587725cf
duhaime <douglas.duhaime@gmail.com>
2016-09-08 11:00:38 -0400

    add font awesome

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 // vendor styles
 require('leaflet/dist/leaflet.css');
 require('angularjs-slider/dist/rzslider.css');
+require('font-awesome-webpack');
 
 // app styles
 require('./css/styles.css');
