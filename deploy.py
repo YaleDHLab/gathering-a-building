0012b1da06972d293f38f7b46c62e6599ead89b6
duhaime <douglas.duhaime@gmail.com>
2016-09-09 08:21:58 -0400

    remove legacy deploy script

--- deploy.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.call('aws s3 cp ../gathering-a-building s3://gathering-a-building-deploy --recursive --exclude=".git*" --exclude="README.md" --exclude="deploy.py" --acl public-read', shell=True)
