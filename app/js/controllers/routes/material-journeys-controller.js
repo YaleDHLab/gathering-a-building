fafb6d0c7794fa44dde36b5b5ded3906d638c6fa
duhaime <douglas.duhaime@gmail.com>
2016-09-16 11:22:34 -0400

    allow landing pages to fade in/out

--- app/js/controllers/routes/material-journeys-controller.js
+++ app/js/controllers/routes/material-journeys-controller.js
@@ -2,8 +2,8 @@ var angular = require('angular');
 
 angular.module('MaterialJourneysController', [])
   .controller("materialJourneysController", [
-      "$scope", "$http",
-  function($scope, $http) {
+      "$scope", "$http", "$timeout",
+  function($scope, $http, $timeout) {
 
     // Set initial footer params, and update as page updates
     $scope.footer = {
@@ -157,41 +157,59 @@ angular.module('MaterialJourneysController', [])
 
     /***
     *
+    * Fades the table of contents into / out of view
+    *
+    ***/
+
+    var showTableOfContents = function(v) {
+      var target = document.querySelector('.table-of-contents-container');
+
+      // if the target exists, update its opacity
+      // else the target doesn't exist in the DOM; add it
+      if (target) {
+        target.style.opacity = v;
+      } else {
+        $scope.showTableOfContents = 1;
+      }
+    }
+
+    /***
+    *
     * Bind a scroll listener to swap background images dynamically
     *
     ***/
 
     $scope.getScrollPosition = function(scrollPosition) {
       if (scrollPosition < 640) {
-        $scope.showTableOfContents = 1;
+        showTableOfContents(1);
         $scope.footer.right.url = "/#/routes/material-journeys#1";
         updateBackground($scope.textColumn["sections"]["0"]["background"]["1"]["url"]);
         $scope.$apply();
       }
 
       if (scrollPosition > 640 && scrollPosition <= 2620) {
-        $scope.showTableOfContents = 0;
+        showTableOfContents(0);
         $scope.footer.right.url = "/#/routes/material-journeys#2";
         updateBackground($scope.textColumn["sections"]["1"]["background"]["1"]["url"]);
         $scope.$apply();
       }
 
       if (scrollPosition > 2620 && scrollPosition <= 4750) {
-        $scope.showTableOfContents = 0;
+        showTableOfContents(0);
         $scope.footer.right.url = "/#/routes/material-journeys#3";
         updateBackground($scope.textColumn["sections"]["2"]["background"]["1"]["url"]);
         $scope.$apply();
       }
 
       if (scrollPosition > 4750 && scrollPosition <= 6670) {
-        $scope.showTableOfContents = 0;
+        showTableOfContents(0);
         $scope.footer.right.url = "/#/routes/material-journeys#4";
         updateBackground($scope.textColumn["sections"]["3"]["background"]["1"]["url"]);
         $scope.$apply();
       }
 
       if (scrollPosition > 6670) {
-        $scope.showTableOfContents = 0;
+        showTableOfContents(0);
         $scope.footer.right.url = "/#/routes/material-journeys#0";
         updateBackground($scope.textColumn["sections"]["4"]["background"]["1"]["url"]);
         $scope.$apply();
@@ -199,7 +217,7 @@ angular.module('MaterialJourneysController', [])
     }
 
     // initialize the application state
-    $scope.showTableOfContents = 1;
+    showTableOfContents(1);
     $scope.backgroundImageUrl = $scope.textColumn["sections"]["0"]["background"]["1"]["url"];
     $scope.buildDropdownOptions();
 
