ed9c23a02061849d8c64a8c5d9bc9613d2512ef1
duhaime <douglas.duhaime@gmail.com>
2016-10-06 09:23:05 -0400

    move home controller to async

--- app/js/controllers/routes/home-controller.js
+++ app/js/controllers/routes/home-controller.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 var angular = require('angular');
+var request = require('superagent');
 
 angular.module('HomeController', [])
   .controller("homeController", [
@@ -29,322 +30,309 @@ angular.module('HomeController', [])
 
   $scope.selectedOverlay = '';
 
-  var overlayData = [
-    {
-      id: 0,
-      xOffset: 0.492,
-      yOffset: 0.26,
-      url: "/#/routes/material-journeys#3",
-      path: "routes/material-journeys",
-      hash: "3",
-      title: "Material Journeys &raquo;",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra metus sapien, et euismod mauris diam, tempus mauris rhoncus nec. (20 words)",
-      image: "/assets/images/chimneys.png"
-    },
-    {
-      id: 1,
-      xOffset: 0.30,
-      yOffset: 0.78,
-      url: "/#/routes/material-journeys#1",
-      path: "routes/material-journeys",
-      hash: "1",
-      title: "Concrete Copula &raquo;",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra metus sapien, et euismod mauris diam, tempus mauris rhoncus nec. (20 words)",
-      image: "/assets/images/concrete.jpg"
-    },
-  ];
+  var endpoint = "http://localhost:8000/json/home.json";
 
-  /***
-  *
-  * Helper function to determine mouse hover position:
-  * used for finding coefficients to position overlays
-  *
-  ***/
+  request
+    .get(endpoint)
+    .set('Accept', 'application/json')
+    .end(function(err, res){
+      if (err) {console.log(err)};
 
-  var findMousePosition = function(evt) {
-    var x = (evt.pageX - $('body').offset().left) + $(window).scrollLeft();
-    var y = (evt.pageY - $('body').offset().top) + $(window).scrollTop();
-  }
+      $scope.overlayIcons = res.body;
 
-  /***
-  *
-  * Public function used to position overlay icons
-  *
-  ***/
+      /***
+      *
+      * Helper function to determine mouse hover position:
+      * used for finding coefficients to position overlays
+      *
+      ***/
 
-  $scope.positionIcons = function() {
-    var overlays = document.querySelectorAll('.building-overlay-marker');
+      var findMousePosition = function(evt) {
+        var x = (evt.pageX - $('body').offset().left) + $(window).scrollLeft();
+        var y = (evt.pageY - $('body').offset().top) + $(window).scrollTop();
+      }
 
-    // obtain data needed for positioning
-    var image = document.querySelector('.home-image');
-    if (image !== null) {
-      var imageHeight = image.clientHeight;
-      var imageWidth = image.clientWidth;
+      /***
+      *
+      * Public function used to position overlay icons
+      *
+      ***/
 
-      var overlayIcon = document.querySelector('.building-overlay-marker');
-      var iconWidth = overlayIcon.clientWidth;
+      $scope.positionIcons = function() {
+        var overlays = document.querySelectorAll('.building-overlay-marker');
 
-      for (var i=0; i<overlays.length; i++) {
-        var overlay = overlays[i];
-        var overlayOffsets = overlayData[i];
+        // obtain data needed for positioning
+        var image = document.querySelector('.home-image');
+        if (image !== null) {
+          var imageHeight = image.clientHeight;
+          var imageWidth = image.clientWidth;
 
-        var xOffset = (overlayOffsets.xOffset * imageWidth) - (iconWidth/2);
-        var yOffset = (overlayOffsets.yOffset * imageHeight) - (iconWidth/2);
+          var overlayIcon = document.querySelector('.building-overlay-marker');
+          if (overlayIcon !== null) {
+            var iconWidth = overlayIcon.clientWidth;
 
-        overlay.style.left = String(xOffset) + "px";
-        overlay.style.top = String(yOffset) + "px";
-        overlay.style.opacity = 1;
+            for (var i=0; i<overlays.length; i++) {
+              var overlay = overlays[i];
+              var overlayOffsets = $scope.overlayIcons[i];
+
+              var xOffset = (overlayOffsets.xOffset * imageWidth) - (iconWidth/2);
+              var yOffset = (overlayOffsets.yOffset * imageHeight) - (iconWidth/2);
+
+              overlay.style.left = String(xOffset) + "px";
+              overlay.style.top = String(yOffset) + "px";
+              overlay.style.opacity = 1;
+            };
+          };
+        };
       };
-    };
-  };
 
-  /***
-  *
-  * Funtion to reposition modal
-  *
-  ***/
+      /***
+      *
+      * Funtion to reposition modal
+      *
+      ***/
 
-  var positionModal = function(initializeModal) {
-    var overlayId = $scope.overlayId;
-    var overlayJson = overlayData[overlayId];
-    var overlayPosition = {};
+      var positionModal = function(initializeModal) {
+        var overlayId = $scope.overlayId;
+        var overlayJson = $scope.overlayIcons[overlayId];
+        var overlayPosition = {};
 
-    $scope.overlayData = overlayJson;
+        $scope.overlayData = overlayJson;
 
-    // set the desired distance between modal icon and modal
-    // and length of modal
-    var modalPadding = 22;
-    var modalLength = 468;
-    var modalHeight = 200;
-    var iconHeight = 40;
+        // set the desired distance between modal icon and modal
+        // and length of modal
+        var modalPadding = 22;
+        var modalLength = 468;
+        var modalHeight = 200;
+        var iconHeight = 40;
 
-    // store the icon the user just clicked
-    var overlayIcon = document.querySelector('.building-overlay-marker-' + overlayId);
-    var iconLeft = parseInt(overlayIcon.style.left, 10);
-    var iconTop = parseInt(overlayIcon.style.top, 10);
+        // store the icon the user just clicked
+        var overlayIcon = document.querySelector('.building-overlay-marker-' + overlayId);
+        var iconLeft = parseInt(overlayIcon.style.left, 10);
+        var iconTop = parseInt(overlayIcon.style.top, 10);
 
-    /****
-    *
-    * Modal positioning logic: divide the screen into a 3x3 grid, with
-    * each grid region along the x-axis occupying 1/3 the image width
-    * and each grid region along the y-axis occupying 1/3 the image height.
-    * Then find the icon the user clicked, and use the grid in which that icon
-    * falls to determine the modal's position vis-a-vis the icon
-    *
-    ***/
+        /****
+        *
+        * Modal positioning logic: divide the screen into a 3x3 grid, with
+        * each grid region along the x-axis occupying 1/3 the image width
+        * and each grid region along the y-axis occupying 1/3 the image height.
+        * Then find the icon the user clicked, and use the grid in which that icon
+        * falls to determine the modal's position vis-a-vis the icon
+        *
+        ***/
 
-    // determine x axis position
-    if (overlayJson.xOffset > .25) {
-      if (overlayJson.xOffset > .75) {
-        overlayPosition.xRegion = 'right';
-      } else {
-        overlayPosition.xRegion = 'mid';
+        // determine x axis position
+        if (overlayJson.xOffset > .25) {
+          if (overlayJson.xOffset > .75) {
+            overlayPosition.xRegion = 'right';
+          } else {
+            overlayPosition.xRegion = 'mid';
+          }
+        } else {
+          overlayPosition.xRegion = 'left';
+        };
+
+        // determine y axis position
+        if (overlayJson.yOffset > .333) {
+          if (overlayJson.yOffset > .666) {
+            overlayPosition.yRegion = 'bottom';
+          } else {
+            overlayPosition.yRegion = 'mid';
+          }
+        } else {
+          overlayPosition.yRegion = 'top';
+        }
+
+        /***
+        *
+        * Given the grid region of the icon, determine the 
+        * position of the modal
+        *
+        ***/
+
+        // establish x axis position of modal
+        if (overlayPosition.xRegion == 'right') {
+          overlayPosition.x = iconLeft - modalLength - modalPadding;
+        }
+        if (overlayPosition.xRegion == 'mid') {
+          overlayPosition.x = iconLeft - (modalLength/2);
+        }
+        if (overlayPosition.xRegion == 'left') {
+          overlayPosition.x = iconLeft + modalPadding;
+        }
+
+        // establish y axis position of modal
+        if (overlayPosition.yRegion == 'bottom') {
+          overlayPosition.y = iconTop - modalHeight - modalPadding;
+        }
+        if (overlayPosition.yRegion == 'mid') {
+          overlayPosition.y = iconTop - (modalHeight/2);
+        }
+        if (overlayPosition.yRegion == 'top') {
+          overlayPosition.y = iconTop + iconHeight + modalPadding - 5;
+        }
+
+        // position the modal
+        var modal = document.querySelector(".building-overlay-modal");
+        modal.style.left = overlayPosition.x + "px";
+        modal.style.top = overlayPosition.y + "px";
+
+        // after positioning the modal, display it if it needs displaying
+        if (initializeModal == 1) {
+          // display the modal by adding opacity
+          var container = document.querySelector('.building-overlay-modal');
+          container.style.opacity = 1;
+        }
       }
-    } else {
-      overlayPosition.xRegion = 'left';
-    };
 
-    // determine y axis position
-    if (overlayJson.yOffset > .333) {
-      if (overlayJson.yOffset > .666) {
-        overlayPosition.yRegion = 'bottom';
-      } else {
-        overlayPosition.yRegion = 'mid';
+      /***
+      *
+      * Private function that scrolls the selected modal into full view
+      * if necessary
+      *
+      ***/
+
+      var scrollToModal = function() {
+        var windowWidth = window.innerWidth;
+        var xScroll = document.querySelector("#home").scrollLeft;
+        var modal = document.querySelector('.building-overlay-modal');
+        var xModal = parseInt(modal.style.left, 10);
+        var modalWidth = modal.clientWidth;
+
+        // if the modal doesn't fit entirely on the screen,
+        // scroll such that it will (and display some padding right)
+        var padding = 75;
+
+        // first check to see if we must scroll right
+        var sightDistance = windowWidth + xScroll;
+        var modalDistance = xModal + modalWidth + padding;
+
+        // calculate the amount we should scroll right
+        var scrollRight = modalDistance - sightDistance;
+        var scrollLeft = xScroll - xModal + padding;
+
+        // use jQuery animation to create smooth scroll
+        if (scrollRight > 0) {
+          $("#home").animate({scrollLeft: xScroll + scrollRight}, 700, "swing");
+          return;
+        }
+
+        // check to see if we must scroll left
+        if (xScroll > xModal) {
+          $("#home").animate({scrollLeft: xScroll - scrollLeft}, 700, "swing");
+          return;
+        }
       }
-    } else {
-      overlayPosition.yRegion = 'top';
-    }
 
-    /***
-    *
-    * Given the grid region of the icon, determine the 
-    * position of the modal
-    *
-    ***/
+      /***
+      *
+      * Private function to determine whether an image is loaded
+      *
+      ***/
 
-    // establish x axis position of modal
-    if (overlayPosition.xRegion == 'right') {
-      overlayPosition.x = iconLeft - modalLength - modalPadding;
-    }
-    if (overlayPosition.xRegion == 'mid') {
-      overlayPosition.x = iconLeft - (modalLength/2);
-    }
-    if (overlayPosition.xRegion == 'left') {
-      overlayPosition.x = iconLeft + modalPadding;
-    }
+      var imageLoaded = function(img) {
+        return img.complete && img.naturalHeight != 0;
+      }
 
-    // establish y axis position of modal
-    if (overlayPosition.yRegion == 'bottom') {
-      overlayPosition.y = iconTop - modalHeight - modalPadding;
-    }
-    if (overlayPosition.yRegion == 'mid') {
-      overlayPosition.y = iconTop - (modalHeight/2);
-    }
-    if (overlayPosition.yRegion == 'top') {
-      overlayPosition.y = iconTop + iconHeight + modalPadding - 5;
-    }
+      /***
+      *
+      * Public function for initializing the overlays after
+      * the home image is loaded
+      *
+      ***/
 
-    // position the modal
-    var modal = document.querySelector(".building-overlay-modal");
-    modal.style.left = overlayPosition.x + "px";
-    modal.style.top = overlayPosition.y + "px";
+      $scope.initializeOverlays = function() {
+        // If the image is already loaded, position the overlays,
+        // else add an onload event that will position the overlays
+        // once the image is loaded
+        var image = document.querySelector(".home-image");
 
-    // after positioning the modal, display it if it needs displaying
-    if (initializeModal == 1) {
-      // display the modal by adding opacity
-      var container = document.querySelector('.building-overlay-modal');
-      container.style.opacity = 1;
-    }
-  }
+        if (imageLoaded(image)) {
+          $scope.positionOverlays();
+        } else {
+          image.addEventListener('load', $scope.positionOverlays, false);
+        }
+      }
 
-  /***
-  *
-  * Private function that scrolls the selected modal into full view
-  * if necessary
-  *
-  ***/
+      /***
+      *
+      * Public function for repositioning icon and modal overlays
+      *
+      ***/
 
-  var scrollToModal = function() {
-    var windowWidth = window.innerWidth;
-    var xScroll = document.querySelector("#home").scrollLeft;
-    var modal = document.querySelector('.building-overlay-modal');
-    var xModal = parseInt(modal.style.left, 10);
-    var modalWidth = modal.clientWidth;
+      $scope.positionOverlays = function() {
+        $scope.positionIcons();
 
-    // if the modal doesn't fit entirely on the screen,
-    // scroll such that it will (and display some padding right)
-    var padding = 75;
+        // only reposition the modal overlay if it exists
+        var overlay = document.querySelector('.building-overlay-modal');
+        if (overlay !== null && overlay.style.opacity == 1) {
+          positionModal(0);
+        }
+      };
 
-    // first check to see if we must scroll right
-    var sightDistance = windowWidth + xScroll;
-    var modalDistance = xModal + modalWidth + padding;
+      /***
+      *
+      * Function called when users click icon overlays
+      *
+      ***/
 
-    // calculate the amount we should scroll right
-    var scrollRight = modalDistance - sightDistance;
-    var scrollLeft = xScroll - xModal + padding;
+      $scope.toggleModal = function(overlayId) {
 
-    // use jQuery animation to create smooth scroll
-    if (scrollRight > 0) {
-      $("#home").animate({scrollLeft: xScroll + scrollRight}, 700, "swing");
-      return;
-    }
+        // store the selected overlay in the global scope,
+        // pass the overlay data to the template, and initialize
+        // the modal
+        $scope.overlayId = overlayId;
+        var overlayJson = $scope.overlayIcons[overlayId];
+        $scope.overlayData = overlayJson;
+        positionModal(1);
+        scrollToModal();
+      }
 
-    // check to see if we must scroll left
-    if (xScroll > xModal) {
-      $("#home").animate({scrollLeft: xScroll - scrollLeft}, 700, "swing");
-      return;
-    }
-  }
+      /***
+      *
+      * Click listener that can catch clicks to close modal and restore
+      * opacity to all icons
+      *
+      ***/
 
-  /***
-  *
-  * Private function to determine whether an image is loaded
-  *
-  ***/
+      $scope.hideModal = function() {
+        var modal = document.querySelector(".building-overlay-modal");
+        modal.style.opacity = 0;
+      }
 
-  var imageLoaded = function(img) {
-    return img.complete && img.naturalHeight != 0;
-  }
+      /***
+      *
+      * Callback for requests to deeplink into application pages
+      *
+      * Takes as input a route from one of the overlayData.url elements,
+      * fades body opacity to white, then sends the user to the requested route
+      *
+      ***/
 
-  /***
-  *
-  * Public function for initializing the overlays after
-  * the home image is loaded
-  *
-  ***/
+      $scope.deeplink = function(path, hash) {
+        var body = document.querySelector('.body');
+        body.style.opacity = 0;
+        $timeout(function() {
+          $location.path(path).hash(hash);
+        }, 1000);
+      }
 
-  $scope.initializeOverlays = function() {
-    // If the image is already loaded, position the overlays,
-    // else add an onload event that will position the overlays
-    // once the image is loaded
-    var image = document.querySelector(".home-image");
+      /***
+      *
+      * Load event listeners
+      *
+      ***/
 
-    if (imageLoaded(image)) {
-      $scope.positionOverlays();
-    } else {
-      image.addEventListener('load', $scope.positionOverlays, false);
-    }
-  }
+      $scope.windowResize = function(params) {
+        $scope.positionOverlays();
+      }
 
-  /***
-  *
-  * Public function for repositioning icon and modal overlays
-  *
-  ***/
+      /***
+      *
+      * Initialize controller state
+      *
+      ***/
 
-  $scope.positionOverlays = function() {
-    $scope.positionIcons();
-
-    // only reposition the modal overlay if it exists
-    var overlay = document.querySelector('.building-overlay-modal');
-    if (overlay !== null && overlay.style.opacity == 1) {
-      positionModal(0);
-    }
-  };
-
-  /***
-  *
-  * Function called when users click icon overlays
-  *
-  ***/
-
-  $scope.toggleModal = function(overlayId) {
-
-    // store the selected overlay in the global scope,
-    // pass the overlay data to the template, and initialize
-    // the modal
-    $scope.overlayId = overlayId;
-    var overlayJson = overlayData[overlayId];
-    $scope.overlayData = overlayJson;
-    positionModal(1);
-    scrollToModal();
-  }
-
-  /***
-  *
-  * Click listener that can catch clicks to close modal and restore
-  * opacity to all icons
-  *
-  ***/
-
-  $scope.hideModal = function() {
-    var modal = document.querySelector(".building-overlay-modal");
-    modal.style.opacity = 0;
-  }
-
-  /***
-  *
-  * Callback for requests to deeplink into application pages
-  *
-  * Takes as input a route from one of the overlayData.url elements,
-  * fades body opacity to white, then sends the user to the requested route
-  *
-  ***/
-
-  $scope.deeplink = function(path, hash) {
-    var body = document.querySelector('.body');
-    body.style.opacity = 0;
-    $timeout(function() {
-      $location.path(path).hash(hash);
-    }, 1000);
-  }
-
-  /***
-  *
-  * Load event listeners
-  *
-  ***/
-
-  $scope.windowResize = function(params) {
-    $scope.positionOverlays();
-  }
-
-  /***
-  *
-  * Initialize controller state
-  *
-  ***/
-
-  backgroundStyle.updateBackgroundStyle({navigationButton: "light", brandIcon: "light"});
-
+      backgroundStyle.updateBackgroundStyle({navigationButton: "light", brandIcon: "light"});
+  });
 }]);
